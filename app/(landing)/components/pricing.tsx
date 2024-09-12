eb58d1f31210b90ed28d2a4330ffe5a95cfb423c
maci-kb24 <macikb8@yahoo.com>
2024-09-12 18:20:22 +0200

    update payment links

--- app/(landing)/components/pricing.tsx
+++ app/(landing)/components/pricing.tsx
@@ -31,7 +31,7 @@ const Pricing = () => (
             <div>/month</div>
           </div>
           <Link
-            href="#"
+            href="https://buy.stripe.com/00gcN12UufslcLu9AC"
             className="bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] from-zinc-700 via-zinc-950 to-zinc-950 rounded-lg text-white/90 px-3.5 py-2.5 relative text-sm font-medium focus:outline-none focus:ring-2 focus:ring-offset-2 transition ease-in-out duration-500 focus:ring-zinc-950 w-full flex text-center justify-center"
           >
             Get Started
@@ -64,7 +64,7 @@ const Pricing = () => (
             <div>/month</div>
           </div>
           <Link
-            href="#"
+            href="https://buy.stripe.com/00gcN12UufslcLu9AC"
             className="bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] from-zinc-700 via-zinc-950 to-zinc-950 rounded-lg text-white/90 px-3.5 py-2.5 relative text-sm font-medium focus:outline-none focus:ring-2 focus:ring-offset-2 transition ease-in-out duration-500 focus:ring-zinc-950 w-full flex text-center justify-center"
           >
             Get Started
@@ -97,7 +97,7 @@ const Pricing = () => (
             <div>/month</div>
           </div>
           <Link
-            href="#"
+            href="https://buy.stripe.com/4gw7sH0Mm5RLeTCdQR"
             className="bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] from-zinc-700 via-zinc-950 to-zinc-950 rounded-lg text-white/90 px-3.5 py-2.5 relative text-sm font-medium focus:outline-none focus:ring-2 focus:ring-offset-2 transition ease-in-out duration-500 focus:ring-zinc-950 w-full flex text-center justify-center"
           >
             Get Started
